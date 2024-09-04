3cb51d20af3984cf36a91ef42945f374810a6355
komil1985 <komil1985@outlook.com>
2024-09-04 19:04:10 +0530

    Initial Project Files

---
+++ Source/LetThereBeLight/LetThereBeLight.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "LetThereBeLight.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LetThereBeLight, "LetThereBeLight" );
