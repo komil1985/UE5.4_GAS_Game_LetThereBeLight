4076ca8e9b3e9440135722a01d5ee8ce279ef7f3
komil1985 <komil1985@outlook.com>
2025-01-26 23:24:26 +0530

    Enemy AI & Polishing

--- Source/LetThereBeLight/Private/AbilitySystem/Abilities/KDSummonAbility.cpp
+++ Source/LetThereBeLight/Private/AbilitySystem/Abilities/KDSummonAbility.cpp
@@ -3,3 +3,33 @@
 
 #include "AbilitySystem/Abilities/KDSummonAbility.h"
 
+TArray<FVector> UKDSummonAbility::GetSpawnLocation()
+{
+	const FVector Forward = GetAvatarActorFromActorInfo()->GetActorForwardVector();
+	const FVector Location = GetAvatarActorFromActorInfo()->GetActorLocation();
+	const float DeltaSpread = SpawnRadius / NumMinions;
+
+	const FVector LeftOfSpread = Forward.RotateAngleAxis(-SpawnRadius / 2.0f, FVector::UpVector);
+	TArray<FVector> SpawnLocations;
+	for (int32 i = 0; i < NumMinions; i++)
+	{
+		const FVector Direction = LeftOfSpread.RotateAngleAxis(DeltaSpread * i, FVector::UpVector);
+		FVector RandomSpawnLocation = Location + Direction * FMath::FRandRange(MinSpawnDistance, MaxSpawnDistance);
+
+		FHitResult Hit;
+		GetWorld()->LineTraceSingleByChannel(Hit, RandomSpawnLocation + FVector(0.0f, 0.0f, 400.0f), RandomSpawnLocation - FVector(0.0f, 0.0f, 400.0f), ECC_Visibility);
+		if (Hit.bBlockingHit)
+		{
+			RandomSpawnLocation = Hit.ImpactPoint;
+		}
+		SpawnLocations.Add(RandomSpawnLocation);
+	}
+
+	return SpawnLocations;
+}
+
+TSubclassOf<APawn> UKDSummonAbility::GetRandomMinionClass()
+{
+	const int32 Selection = FMath::RandRange(0, MinionClasses.Num() - 1);
+	return MinionClasses[Selection];
+}
--- Source/LetThereBeLight/Public/AbilitySystem/Abilities/KDSummonAbility.h
+++ Source/LetThereBeLight/Public/AbilitySystem/Abilities/KDSummonAbility.h
@@ -13,5 +13,25 @@ UCLASS()
 class LETTHEREBELIGHT_API UKDSummonAbility : public UKDGameplayAbility
 {
 	GENERATED_BODY()
-	
+public:
+	UFUNCTION(BlueprintCallable)
+	TArray<FVector> GetSpawnLocation();
+
+	UFUNCTION(BlueprintPure, Category = "Summoning")
+	TSubclassOf<APawn> GetRandomMinionClass();
+
+	UPROPERTY(EditDefaultsOnly, Category = "Summoning")
+	int32 NumMinions = 5;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Summoning")
+	TArray<TSubclassOf<APawn>> MinionClasses;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Summoning")
+	float MinSpawnDistance = 50.0f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Summoning")
+	float MaxSpawnDistance = 250.0f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Summoning")
+	float SpawnRadius = 90.0f;
 };
