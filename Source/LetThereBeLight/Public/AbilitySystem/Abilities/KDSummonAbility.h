72e880359ea5f42e5a9312076f7d3c0e5e4ff2e5
komil1985 <komil1985@outlook.com>
2025-01-26 22:29:12 +0530

    Enemy AI & Polishing

---
+++ Source/LetThereBeLight/Public/AbilitySystem/Abilities/KDSummonAbility.h
@@ -0,0 +1,17 @@
+// Copyright ASKD Games
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/KDGameplayAbility.h"
+#include "KDSummonAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LETTHEREBELIGHT_API UKDSummonAbility : public UKDGameplayAbility
+{
+	GENERATED_BODY()
+	
+};
