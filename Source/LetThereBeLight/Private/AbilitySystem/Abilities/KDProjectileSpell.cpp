50373edb4768ebfd56206569307bde543a66b3a1
komil1985 <komil1985@outlook.com>
2025-02-02 16:16:43 +0530

    Level Tweaks

--- Source/LetThereBeLight/Private/AbilitySystem/Abilities/KDProjectileSpell.cpp
+++ Source/LetThereBeLight/Private/AbilitySystem/Abilities/KDProjectileSpell.cpp
@@ -9,6 +9,7 @@
 #include "Misc/KDGameplayTags.h"
 
 
+
 void UKDProjectileSpell::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
 {
 	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
