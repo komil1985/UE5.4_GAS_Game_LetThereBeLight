11ce9dcbc51a1d2170ad7e5cd8c3d6811fe5967d
komil1985 <komil1985@outlook.com>
2025-02-17 22:18:41 +0530

    Experience & Leveling Up

---
+++ Source/LetThereBeLight/Public/AbilitySystem/Data/LevelUpInfo.h
@@ -0,0 +1,39 @@
+// Copyright ASKD Games
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "LevelUpInfo.generated.h"
+
+
+USTRUCT(BlueprintType)
+struct FKDLevelUpInfo
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly)
+	int32 LevelUpRequirement = 0;
+
+	UPROPERTY(EditDefaultsOnly)
+	int32 AttributePointAward = 1;
+
+	UPROPERTY(EditDefaultsOnly)
+	int32 SpellPointAward = 1;
+
+};
+
+/**
+ * 
+ */
+UCLASS()
+class LETTHEREBELIGHT_API ULevelUpInfo : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly)
+	TArray<FKDLevelUpInfo> LevelUpInformation;
+
+	int32 FindLevelForXP(int32 XP);
+};
