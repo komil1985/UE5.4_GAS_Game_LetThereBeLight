5a5d458b292db65b3729c9b50b8f0a5312e2c33d
komil1985 <komil1985@outlook.com>
2025-04-03 22:20:49 +0530

    Fire Blast

--- Source/LetThereBeLight/Private/AbilitySystem/Abilities/KDFireBlast.cpp
+++ Source/LetThereBeLight/Private/AbilitySystem/Abilities/KDFireBlast.cpp
@@ -36,5 +36,30 @@ FString UKDFireBlast::GetDescription(int32 Level)
 
 FString UKDFireBlast::GetNextLevelDescription(int32 Level)
 {
-	return GetDescription(Level);
+	const int32 ScaledDamage = Damage.GetValueAtLevel(Level);
+	const float ManaCost = FMath::Abs(GetManaCost(Level));
+	const float Cooldown = GetCooldown(Level);
+
+
+	return FString::Printf(TEXT(
+		// Title
+		"<Title>NEXT LEVEL</>\n\n"
+
+		// Details
+		"<Small>Level: </><Level>%d</>\n"
+		"<Small>ManaCost: </><ManaCost>%.1f</>\n"
+		"<Small>Cooldown: </><Cooldown>%.1f</>\n\n"
+
+		// Description
+		"<Default>Launches %d </>"
+		"<Default>fire balls in all directions, each coming back and </>"
+		"<Default>exploding upon return causing </>"
+		"<Damage>%d</>"
+		"<Default> radial fire damage with a chance to burn</>"),
+
+		// Values
+		Level,
+		ManaCost,
+		Cooldown,
+		ScaledDamage);
 }
