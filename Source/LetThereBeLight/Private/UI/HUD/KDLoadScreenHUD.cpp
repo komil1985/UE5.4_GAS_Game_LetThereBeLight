d981ce3c9ae04dfe35bbeb17c4b8495131afa2a5
komil1985 <komil1985@outlook.com>
2025-04-11 07:58:21 +0530

    Saving System

--- Source/LetThereBeLight/Private/UI/HUD/KDLoadScreenHUD.cpp
+++ Source/LetThereBeLight/Private/UI/HUD/KDLoadScreenHUD.cpp
@@ -2,4 +2,18 @@
 
 
 #include "UI/HUD/KDLoadScreenHUD.h"
+#include "Blueprint/UserWidget.h"
+#include "UI/Widget/KDLoadScreenWidget.h"
+#include "UI/ViewModel/KD_MVVM_LoadScreen.h"
 
+
+void AKDLoadScreenHUD::BeginPlay()
+{
+	Super::BeginPlay();
+
+	LoadScreenViewModel = NewObject<UKD_MVVM_LoadScreen> (this, LoadScreenViewModelClass);
+
+	LoadScreenWidget = CreateWidget<UKDLoadScreenWidget>(GetWorld(), LoadScreenWidgetClass);
+	LoadScreenWidget->AddToViewport();
+
+}
--- Source/LetThereBeLight/Public/UI/HUD/KDLoadScreenHUD.h
+++ Source/LetThereBeLight/Public/UI/HUD/KDLoadScreenHUD.h
@@ -6,6 +6,9 @@
 #include "GameFramework/HUD.h"
 #include "KDLoadScreenHUD.generated.h"
 
+class UKDLoadScreenWidget;
+class UKD_MVVM_LoadScreen;
+
 /**
  * 
  */
@@ -13,5 +16,22 @@ UCLASS()
 class LETTHEREBELIGHT_API AKDLoadScreenHUD : public AHUD
 {
 	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<UUserWidget> LoadScreenWidgetClass;
+
+	UPROPERTY(BlueprintReadOnly)
+	TObjectPtr<UKDLoadScreenWidget>LoadScreenWidget;
+
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<UKD_MVVM_LoadScreen> LoadScreenViewModelClass;
+
+	UPROPERTY(BlueprintReadOnly)
+	TObjectPtr<UKD_MVVM_LoadScreen> LoadScreenViewModel;
+
+
+protected:
+	virtual void BeginPlay() override;
 	
 };
