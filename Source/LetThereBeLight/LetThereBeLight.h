0580e98957d7e44cc9c98352f27de7370451374f
komil1985 <komil1985@outlook.com>
2025-04-23 07:48:04 +0530

    Checkpoint System

--- Source/LetThereBeLight/LetThereBeLight.h
+++ Source/LetThereBeLight/LetThereBeLight.h
@@ -5,6 +5,10 @@
 #include "CoreMinimal.h"
 
 #define CUSTOM_DEPTH_RED 250
+#define CUSTOM_DEPTH_BLUE 251
+#define CUSTOM_DEPTH_TAN 252
+
+
 
 #define ECC_Projectile ECollisionChannel::ECC_GameTraceChannel1
 
